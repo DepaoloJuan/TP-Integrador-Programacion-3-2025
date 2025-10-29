4735f91319f4344f006f6aa5a71295568d09e4ed
Depaolo Juan Manuel <depaolojuanmanuel@icloud.com>
2025-10-29 16:55:13 -0300

    pagina bienvenida

---
+++ frontend/bienvenida.js
@@ -0,0 +1,15 @@
+// Elementos del DOM  obtener datos del formulario
+const formNombre = document.getElementById("form-nombre");
+const inputNombre = document.getElementById("input-nombre");
+
+// escuchar el boton ingresar
+formNombre.addEventListener("submit", function (event) {
+  event.preventDefault(); //hace que el formulario no recargue la pagina sin esto no funciona!!!
+  const nombreUsuario = inputNombre.value.trim(); // agarramos el nombre y le quitamos los espacios
+  if (nombreUsuario === "") {
+    alert("Por favor, ingresa un nombre válido.");
+  }
+  localStorage.setItem("nombreUsuario", nombreUsuario); // guardamos el nombre en localstorage
+
+  window.location.href = "../index.html"; //nos lleva a la pagina principal
+});
