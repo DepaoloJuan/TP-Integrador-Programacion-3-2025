94f2685fbfefb9b26f4ca6f4065bb154cf1ecc84
Alejo Podbielski <124207059+Valentinpodbielski@users.noreply.github.com>
2025-11-18 08:28:46 -0300

    Carpetas y archivos nuevos

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import environments from "./src/api/config/environments.js"; // Variables de ent
 import cors from "cors"; // Permite que el front haga peticiones al back
 import { loggerUrl } from "./src/api/middlewares/middlewares.js";
 import { productRoutes } from "./src/api/routes/index.js";
+import { join, __dirname } from "./src/api/utils/index.js";
 
 const app = express(); // Crea una instancia de Express.
 const PORT = environments.PORT; // Guarda el puerto donde va a escuchar el servidor.
@@ -13,15 +14,40 @@ app.use(cors());
 app.use(express.json()); //trasnformar el json del post a objeto de js
 app.use(loggerUrl);
 
+
+// Middleware para servir archivos estaticos
+app.use(express.static(join(__dirname, "src/public"))); // Vamos a construir la ruta relativa para servir los archivos de la carpeta /public
+
+/*=====================
+    Configuracion
+====================*/
+app.set("view engine", "ejs"); // Configuramos EJS como motor de plantillas
+app.set("views", join(__dirname, "src/views")); // Indicamos la ruta de las vistas en nuestro proyecto
+
+
+/*==================
+    Rutas
+==================*/
+
+// Endpoint que no devuelve ninguna respuesta y queda la llamada colgada y la conexion sin terminar
+app.get("/test", (req, res) => {
+    console.log("Este endpoint no ofrece ninguna respuesta y se queda aca trabado...");
+});
+
+
 // iniciamos el servidor
 app.listen(PORT, () => {
   // Pone al servidor a escuchar peticiones en el puerto indicado.
   console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
 
-// Rutas
 
-app.use("/api/products", productRoutes);
+
+app.use("/api/products", productRoutes); // Rutas
+
+app.get("/dashboard", (req, res) => {
+    res.render("index");
+})
 
 //app.use("/api/users", rutasUsuario);
 
