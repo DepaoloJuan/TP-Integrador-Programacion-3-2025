07267f31a2fb8ef29291af61b812eeae17a304ff
Alejo Podbielski <124207059+Valentinpodbielski@users.noreply.github.com>
2025-11-29 19:07:15 -0300

    modularizacion del loggin

---
+++ backend/src/public/js/loggin.js
@@ -0,0 +1,9 @@
+
+let emailUser = document.getElementById("emailUser");
+let passwordUser = document.getElementById("passwordUser");
+
+let acceso_rapido = document.getElementById("acceso");
+acceso_rapido.addEventListener("click", () => {
+    emailUser.value = "kiki@gmail.com";
+    passwordUser.value = "1707";
+});
